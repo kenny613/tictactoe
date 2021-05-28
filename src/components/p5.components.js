43abac742d4f62f0b7ab23bc0e588506cd06d8ae
Kenny Tong <tongkai0613@gmail.com>
2021-05-29 04:41:52 +0800

    update using state

--- src/components/p5.components.js
+++ src/components/p5.components.js
@@ -6,133 +6,148 @@ export default class P5 extends Component {
     super(props);
     this.state = {
       finish: false,
-      statecount: 0,
+      count: 0,
+      w:0,
+      h:0,
+      board : [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+      ]
     };
   }
+
+  setup = (p5, parentRef) => {
+    // use parent to render the canvas in this ref
+    // (without that p5 will render the canvas outside of your component)
+    p5.createCanvas(400, 400).parent(parentRef);
+    this.setState((state)=>({
+      w:p5.width / 3,
+      h:p5.height / 3
+    }))
+   
+  };
+
+  draw = (p5) => {
+    for (var i = 0; i <= p5.width; i += this.state.w) {
+      for (var j = 0; j <= p5.height; j += this.state.h) {
+        //draw the grid line
+        p5.stroke(0);
+        p5.strokeWeight(1);
+        p5.line(i, 0, i, p5.height);
+        p5.line(0, j, p5.width, j);
+        //draw the X
+        p5.textSize(this.state.w);
+      }
+    }
+    p5.line(0, p5.height-1, p5.width, p5.height-1);
+  };
+
+  updateCount = () => {
+    this.setState((state) => ({
+      finish: true,
+      count: state.count,
+    }));
+  };
+
+  addCount = () => {
+    this.setState((state) => ({
+      count: state.count+1,
+    }));
+  };
+
+  mouseClicked = (p5) => {
+    let x = Math.floor(p5.mouseX / this.state.w);
+    let y = Math.floor(p5.mouseY / this.state.h);
+    if (this.state.board[x][y] === "") {
+      p5.textSize(this.state.w);
+      let a;
+      let b = y * this.state.h + this.state.h - this.state.h / 8;
+
+      if (this.state.count % 2 === 0) {
+        a = x * this.state.w + this.state.w / 8;
+        let board = this.state.board;
+        board[x][y]="O"
+        this.setState(() => ({
+          board: board,
+        }));
+      } else {
+        a = x * this.state.w + this.state.w / 6;
+        let board = this.state.board;
+        board[x][y]="X"
+        this.setState(() => ({
+          board: board,
+        }));
+      }
+
+      p5.text(this.state.count % 2 === 0 ? "O" : "X", a, b);
+
+      this.addCount()
+
+      if (this.checkwin(this.state.board)) {
+        return <div>Won!</div>;
+      }
+    }
+  };
+
+  checkwin = (board) => {
+    for (var i = 0; i < 3; i++) {
+      if (
+        this.state.board[i][0] === this.state.board[i][1] &&
+        this.state.board[i][1] === this.state.board[i][2] &&
+        (this.state.board[i][0] === "O" || this.state.board[i][0] === "X")
+      ) {
+        this.updateCount();
+      } else if (
+        this.state.board[0][i] === this.state.board[1][i] &&
+        this.state.board[1][i] === this.state.board[2][i] &&
+        (this.state.board[0][i] === "O" || this.state.board[0][i] === "X")
+      ) {
+        this.updateCount();
+      } else if (
+        this.state.board[0][0] === this.state.board[1][1] &&
+        this.state.board[1][1] === this.state.board[2][2] &&
+        (this.state.board[0][0] === "O" || this.state.board[0][0] === "X")
+      ) {
+        this.updateCount();
+      } else if (
+        this.state.board[0][2] === this.state.board[1][1] &&
+        this.state.board[1][1] ===this.state.board[2][0] &&
+        (this.state.board[0][2] === "O" || this.state.board[0][2] === "X")
+      ) {
+        this.updateCount();
+      }
+    }
+    for (var a = 0; a < 3; a++) {
+      for (var b = 0; b < 3; b++) {
+        if (this.state.board[a][b] === "") {
+          return false;
+        }
+      }
+    }
+    this.updateCount();
+  };
+
   render() {
-    let w;
-    let h;
-    let count = 0;
-    let board = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-    ];
-
-    const setup = (p5, parentRef) => {
-      // use parent to render the canvas in this ref
-      // (without that p5 will render the canvas outside of your component)
-      p5.createCanvas(400, 400).parent(parentRef);
-      w = p5.width / 3;
-      h = p5.height / 3;
-    };
-
-    const draw = (p5) => {
-      // p5.background(220);
-
-      for (var i = 0; i <= p5.width; i += w) {
-        for (var j = 0; j <= p5.height; j += h) {
-          //draw the grid line
-          p5.stroke(0);
-          p5.strokeWeight(1);
-          p5.line(i, 0, i, p5.height);
-          p5.line(0, j, p5.width, j);
-          //draw the X
-          p5.textSize(w);
-        }
-      }
-    };
-    const updateCount = () => {
-      this.setState((state) => ({
-        finish: true,
-        statecount: count,
-      }));
-      console.log(this.state.statecount);
-    };
-
-    const mouseClicked = (p5) => {
-      let x = Math.floor(p5.mouseX / w);
-      let y = Math.floor(p5.mouseY / h);
-      if (board[x][y] === "") {
-        p5.textSize(w);
-        let a;
-        let b = y * h + h - h / 8;
-
-        if (count % 2 === 0) {
-          a = x * w + w / 8;
-          board[x][y] = "O";
-        } else {
-          a = x * w + w / 6;
-          board[x][y] = "X";
-        }
-
-        p5.text(count % 2 === 0 ? "O" : "X", a, b);
-
-        count++;
-
-        console.log(board[x][y]);
-        if (checkwin(board)) {
-          return <div>Won!</div>;
-        }
-      }
-    };
-
-    const checkwin = (board) => {
-      for (var i = 0; i < 3; i++) {
-        if (
-          board[i][0] === board[i][1] &&
-          board[i][1] === board[i][2] &&
-          (board[i][0] === "O" || board[i][0] === "X")
-        ) {
-          updateCount();
-        } else if (
-          board[0][i] === board[1][i] &&
-          board[1][i] === board[2][i] &&
-          (board[0][i] === "O" || board[0][i] === "X")
-        ) {
-          updateCount();
-        } else if (
-          board[0][0] === board[1][1] &&
-          board[1][1] === board[2][2] &&
-          (board[0][0] === "O" || board[0][0] === "X")
-        ) {
-          updateCount();
-        } else if (
-          board[0][2] === board[1][1] &&
-          board[1][1] === board[2][0] &&
-          (board[0][2] === "O" || board[0][2] === "X")
-        ) {
-          updateCount();
-        }
-      }
-      for (var a = 0; a < 3; a++) {
-        for (var b = 0; b < 3; b++) {
-          if (board[a][b] === "") {
-            return false;
-          }
-        }
-      }
-      updateCount();
-    };
-
     return (
       <React.Fragment>
         <Sketch
-          setup={setup}
-          draw={draw}
-          mouseClicked={this.state.finish ? 1 : mouseClicked}
+          setup={this.setup}
+          draw={this.draw}
+          mouseClicked={this.state.finish ? ()=>{} : this.mouseClicked}
         />
         <div>
-          {this.state.statecount === 0 && <div>Click to put your symbol!</div>}
+          {this.state.count === 0 && <div>Click to put your symbol!</div>}
+          <div>This is {this.state.count % 2 === 0 && "O"} 
+          {this.state.count % 2 != 0 && "X"}'s turn  </div>
           The game is {this.state.finish ? " finished." : " going on"}
-          {this.state.finish && this.state.statecount !== 9 && (
+          {this.state.finish && this.state.count !== 9 && (
             <div>
-              The game winner is {this.state.statecount % 2 === 0 ? "X" : "O"}{" "}
+              The game winner is {this.state.count % 2 === 0 ? "X" : "O"}{" "}
             </div>
           )}
-          {this.state.finish && this.state.statecount === 9 && <div>Tie!</div>}
+          {this.state.finish && this.state.count === 9 && <div>Tie!</div>}
           <br></br>
-          {board[0][2]}
         </div>
       </React.Fragment>
     );
